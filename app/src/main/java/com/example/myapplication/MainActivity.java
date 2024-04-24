b8fb8febf6893258baf5be5d7b1bd3c81a795381
SureshTimma <148480722+SureshTimma@users.noreply.github.com>
2024-04-24 19:37:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Make sure your layout file matches
+
+        myWebView = findViewById(R.id.myWebView);
+        WebSettings webSettings = myWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient()); // Keeps links inside the WebView
+        myWebView.loadUrl("url.com");
+    }
+}
